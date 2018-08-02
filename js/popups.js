5c7e6482bd8ccc5a4be7045aa9a76f2ffb028268
Boris Larchenko <b.larchenko@pulsarfour.com>
2018-08-02 10:10:25 +0300

    add animation for modal-login

--- js/popups.js
+++ js/popups.js
@@ -5,11 +5,13 @@ var loginClose = loginForm.querySelector('.modal-close');
 loginLink.addEventListener('click', function (evt) {
   evt.preventDefault();
   loginForm.classList.remove('display-none');
+  loginForm.classList.add('modal-login-animate');
 });
 
 loginClose.addEventListener('click', function (evt) {
   evt.preventDefault();
   loginForm.classList.add('display-none');
+  loginForm.classList.remove('modal-login-animate');
 });
 
 var mapLink = document.querySelector(".call-map");
