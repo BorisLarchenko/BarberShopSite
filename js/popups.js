530ae8b575e70fd525eb89b779741e286e626157
Boris Larchenko <b.larchenko@pulsarfour.com>
2018-08-23 22:35:20 +0300

    add js for login in shop.html

--- js/popups.js
+++ js/popups.js
@@ -1,17 +1,20 @@
 var loginLink = document.querySelector(".user-block");
 var loginForm = document.querySelector(".modal-login");
 var loginClose = loginForm.querySelector('.modal-close');
+var overlay = document.querySelector(".modal-overlay");
 
 loginLink.addEventListener('click', function (evt) {
   evt.preventDefault();
   loginForm.classList.remove('display-none');
   loginForm.classList.add('modal-login-animate');
+  overlay.classList.remove('display-none');
 });
 
 loginClose.addEventListener('click', function (evt) {
   evt.preventDefault();
   loginForm.classList.add('display-none');
   loginForm.classList.remove('modal-login-animate');
+  overlay.classList.add('display-none');
 });
 
 var mapLink = document.querySelector(".call-map");
@@ -21,9 +24,11 @@ var mapClose = popupMap.querySelector('.modal-close');
 mapLink.addEventListener('click', function (evt) {
   evt.preventDefault();
   popupMap.classList.remove('display-none');
+  overlay.classList.remove('display-none');
 });
 
 mapClose.addEventListener('click', function (evt) {
   evt.preventDefault();
   popupMap.classList.add('display-none');
+  overlay.classList.add('display-none');
 });
